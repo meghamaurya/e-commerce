beb04c6eddaedee6ec1fbd5c8c2716fca3a041b6
megha <meghamini40@gmail.com>
2022-09-02 23:53:39 +0530

    product description page for product component

---
+++ my-app/src/Components/ProductDescription/index.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import './styles.css';
+export default function ProductDescription() {
+    const [product, setProduct] = useState({});
+    const params = useParams();
+    const { productId } = params;
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/${productId}`)
+            .then(res => res.json())
+            .then(json => setProduct(json))
+    }, [productId])
+    return (
+        <div>
+            <h2>{product.title}</h2>
+            <div className="productItem">
+                <img className="img" src={product.image} alt={product.title} width="300px" />
+                <p className="category">{product.category}</p>
+                <p className="description">{product.description}</p>
+                <b className="price">Price: {product.price} $</b>
+            </div>
+        </div>
+    )
+}
