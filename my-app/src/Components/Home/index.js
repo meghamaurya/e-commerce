6cfcdac03896c3f18ca31dd1398619f07fa4c68e
megha <meghamini40@gmail.com>
2022-09-02 23:50:02 +0530

    create home component

---
+++ my-app/src/Components/Home/index.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import Product from './product.json';
+import ProductItem from '../ProductItem';
+import './styles.css';
+export default function Home(props) {
+    const productElements = Product.map((prod, i) => {
+        return (
+            <Link to={`/${prod.id}`} state={{ from: "/" }}>
+                <ProductItem key={i} product={prod} />
+            </Link>
+
+        )
+    })
+    return (
+        <div>
+            <div>welcome {props.user}</div>
+            <div className='product-listing'>{productElements}</div>
+        </div>
+    )
+}
