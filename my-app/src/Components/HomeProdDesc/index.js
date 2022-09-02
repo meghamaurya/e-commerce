f2212078cc5015e550fa5d92f9bf4972041ce601
megha <meghamini40@gmail.com>
2022-09-02 23:50:48 +0530

    product description page for home

---
+++ my-app/src/Components/HomeProdDesc/index.js
@@ -0,0 +1,19 @@
+import { useLocation, useParams, Link } from "react-router-dom";
+import products from "../Home/product.json";
+export default function HomeProdDesc() {
+    const location = useLocation();
+    console.log("props", location);
+    const params = useParams();
+    const prod = products.find((p) => p.id == params.id);
+    return (
+        <div key={params.id}>
+            <Link to={location.state ? location.state.from : "/"}>
+                <span>back</span>
+            </Link>
+            <h1>{prod.title}</h1>
+            <img alt={prod.title} src={prod.image} width="200" />
+            <p>{prod.description}</p>
+            <b>Price: {prod.price}</b>
+        </div>
+    )
+}
