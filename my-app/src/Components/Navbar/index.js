5b0bfdd125fb8ab01db5a8cf94b8b2db2fa71a83
megha <meghamini40@gmail.com>
2022-09-02 23:49:29 +0530

    build navbar and style it

---
+++ my-app/src/Components/Navbar/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./styles.css";
+export default function Navbar(props) {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        props.setLogin(false);
+        props.setUser("");
+        navigate("/signin");
+    }
+    return (
+        <div>
+            <navbar className="navbar">
+                <div className="head">
+                    <a>M</a>
+                </div>
+                <input
+                    className="searchbar"
+                    type="text"
+                    placeholder="Search"
+                    name="search"
+                />
+                <div className="product">
+                    <Link to={"/products"} style={{ color: "white", padding: "5px" }}>
+                        Product
+                    </Link>
+                </div>
+                <Link to={"/"} style={{ color: "white", padding: "10px" }}>
+                    Home
+                </Link>
+                {!props.isLogin ? (
+                    <Link to={"/signin"}>
+                        <button className="signUp-btn" type="submit">
+                            Sign In
+                        </button>
+                    </Link>
+                ) : (
+                    <button onClick={handleLogout} className="signUp-btn" type="submit">
+                        Sign Out
+                    </button>
+                )}
+
+            </navbar>
+        </div>
+    );
+}
