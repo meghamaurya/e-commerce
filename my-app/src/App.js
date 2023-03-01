33269dce6ee682b8aab2fd00bb937db4bb0af9ef
megha <meghamini40@gmail.com>
2023-03-01 17:49:17 +0530

    added router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/index';
+import Products from './Components/Products/index'
+import ProductDescription from './Components/ProductDescription';
+import HomeProdDesc from './Components/HomeProdDesc';
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/homeproddesc/:productId' element={<HomeProdDesc />} />
+        <Route path='/products' element={<Products />} />
+        {/* <Route path='/products/:productId' element={<ProductDescription />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
